868b3eec6dce6d2b13965f694e9b64bd46b18f88
Angela Xue <y.angela.xue@gmail.com>
2026-04-09 13:30:28 +1200

    calculate and save rho_c

--- IA2-calccentres.py
+++ IA2-calccentres.py
@@ -4,6 +4,8 @@ Calculate and save cluster centres from the IA2 clusters.
 
 if __name__=="__main__":
 
+    import numpy as np
+    import os
     from utils.enzo import IA2Data, IA2
 
     Paths = [
@@ -13,13 +15,23 @@ if __name__=="__main__":
         "/nesi/nobackup/vuw04655/IA2/E5A-M",
     ]
 
+    c = 1
+
     for Path in Paths:
 
         ia2 = IA2Data(Path)
+        rho_cs = []
 
         for s in IA2.snapshots:
             try:
-                ia2.get_centre(s)
+                centre = ia2.get_centre(s,_c=c)
             except:
                 continue
+            
+            rho_c.append([s])
+            rho_c = ia2.get_val('Density',s,c=c,slc=int(centre[0]))[int(centre[1]),int(centre[2])]
+            rho_cs[-1].append(rho_c)
+        
+        np.savetxt(os.path.join(Path,'rho_c.txt'),rho_cs,header='snapshot rho_c')
+            
 
