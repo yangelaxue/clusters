37569b41b7c970ab43f7f8eed135f79997fc4f7e
Angela Xue <y.angela.xue@gmail.com>
2026-06-04 14:12:54 +1200

    Made this more robust to errors and already completed steps

--- IA2-interpdms.py
+++ IA2-interpdms.py
@@ -2,9 +2,7 @@ import numpy as np
 import h5py, os
 from scipy.interpolate import griddata
 
-def smooth_dm(dm,snapshot):
-    
-    # dm = ia2.get_val('Dark_Matter_Density',snapshot,units='code',c=20)
+def smooth_dm(dm):
     
     shape = dm.shape
     xyz = tuple(np.arange(sh) for sh in shape)
@@ -17,8 +15,36 @@ def smooth_dm(dm,snapshot):
     dm_smooth[np.logical_not(np.isfinite(dm_smooth))] = 0
     dm_smooth = dm_smooth/dm_smooth.sum()*dm.sum()
 
-    with h5py.File(os.path.join(Path,f'dm_smooth_{c}.hdf5'), 'a') as f:
-        f.create_dataset(str(snapshot), data=dm_smooth)
+    return dm_smooth
+
+def loop_success(sns):
+
+    dms = ia2.gen_vals('Dark_Matter_Density',snapshots=sns,units='code',c=c)
+
+    idx = 0
+    for s in sns:
+        
+        if os.path.exists(saveName):
+            with h5py.File(saveName, 'r') as f:
+                if str(s) in f.keys():
+                    print(f"Interpolated DM for snapshot {s} is already calculated.")
+                    idx += 1
+                    continue
+        try:
+            dm = next(dms)
+        except:
+            print(f"Error occured for snapshot={s}. Skipping.")
+            if len(sns)>idx+1:
+                return sns[idx+1:]
+            else:
+                print("Success.")
+                return []
+        dm_smooth = smooth_dm(dm)
+        print(f'Saving interpolation for snapshot={s}.')
+        with h5py.File(saveName, 'a') as f:
+            f.create_dataset(str(s), data=dm_smooth)
+        idx += 1
+    return []
 
 if __name__=="__main__":
 
@@ -34,23 +60,37 @@ if __name__=="__main__":
     assert os.path.exists(args.Path), "First argument MUST be an existing path."
     Path = args.Path
     c = args.c
-    if not args.snapshots:
-        snapshots = IA2.snapshots
-    elif args.snapshots!='all':
+    snapshots = args.snapshots if args.snapshots else 'all'
+    if snapshots=='all':
+        snapshots = list(IA2.snapshots)
+    else:
         snapshots = args.snapshots.removeprefix('[').removesuffix(']')
         snapshots = [int(s) for s in snapshots.split(',')]
     
+
+    saveName = os.path.join(Path,f'dm_smooth_{c}.hdf5')
     ia2 = IA2Data(Path)
+    
+    # If snapshot has already been interpolated, remove it from the snapshots.
+    if os.path.exists(saveName):
+        tmp = []
+        with h5py.File(saveName, 'r') as f:
+            for s in snapshots:
+                if str(s) not in f.keys():
+                    tmp.append(s)
+                else:
+                    print(f"Already interpolated snapshot {s}.")
+        snapshots = tmp
+    # If corresponding file does not exist, remove snapshot from snapshots.
     tmp = []
     fNames = ia2.get_fnames('dt')['dt']
     for s in snapshots:
         for fName in fNames:
-            if fName.endswith(str(s)):
+            if fName.endswith(('000'+str(s))[-3:]):
                 tmp.append(s)
-                continue
     snapshots = tmp
 
-    dms = ia2.gen_vals('Dark_Matter_Density',snapshots=snapshots,units='code',c=c)
-    for s,dm in zip(snapshots,dms):
-        print(f'Interpolating for snapshot={s}.')
-        smooth_dm(dm,s)
+    sns = snapshots
+    while sns:
+        sns = loop_success(sns)
+    
